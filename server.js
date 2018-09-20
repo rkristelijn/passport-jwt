26c733fefd1b8bdfb81b4de0b11167a29201380f
Remi Kristelijn <remi.kristelijn@accenture.com>
2018-09-20 09:24:35 +0200

    updated bcrypt

--- server.js
+++ server.js
@@ -52,6 +52,65 @@ apiRoutes.post('/signup', function (req, res) {
     }
 });
 
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+apiRoutes.post('/authenticate', function (req, res) {
+    User.findOne({
+        name: req.body.name
+    }, function (err, user) {
+        if (err) throw err;
+
+        if (!user) {
+            res.send({ success: false, msg: 'Authentication failed. User not found.' });
+        } else {
+            // check if password matches
+            user.comparePassword(req.body.password, function (err, isMatch) {
+                if (isMatch && !err) {
+                    // if user is found and password is right create a token
+                    var token = jwt.encode(user, config.secret);
+                    // return the information including token as JSON
+                    res.json({ success: true, token: 'JWT ' + token });
+                } else {
+                    res.send({ success: false, msg: 'Authentication failed. Wrong password.' });
+                }
+            });
+        }
+    });
+});
+
+// route to a restricted info (GET http://localhost:8080/api/memberinfo)
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', { session: false }), function (req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+        var decoded = jwt.decode(token, config.secret);
+        User.findOne({
+            name: decoded.name
+        }, function (err, user) {
+            if (err) throw err;
+
+            if (!user) {
+                return res.status(403).send({ success: false, msg: 'Authentication failed. User not found.' });
+            } else {
+                res.json({ success: true, msg: 'Welcome in the member area ' + user.name + '!' });
+            }
+        });
+    } else {
+        return res.status(403).send({ success: false, msg: 'No token provided.' });
+    }
+});
+
+getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
+
 // connect the api routes under /api/*
 app.use('/api', apiRoutes);
 
